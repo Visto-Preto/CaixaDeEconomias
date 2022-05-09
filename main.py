5e8fec0f296c32ca848e2fe43779da9f1a5d629e
Visto-Preto <evppdepf@protonmail.ch>
2022-05-09 16:12:08 -0300

    add db

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #coding: utf-8
 __author__ = 'Visto-Preto'
 
-import os
+import os, sqlite3
 from datetime import datetime
 from module.realsymbol import Real as rs
 
@@ -13,11 +13,46 @@ magenta = '\033[1;35m'
 cyan = '\033[1;36m'
 cls = '\033[m'
 
+
+def ver():
+    if os.path.isfile('settings/cde.db'):
+        pass
+    else:
+        con = sqlite3.connect('settings/cde.db')
+        cur = con.cursor()
+        cur.execute('''CREATE TABLE movimentacao(Data date, TMov text, VMov real)''')
+        con.commit()
+        con.close()
+
+def v_conta():
+
+    ver()
+    def del_car(x):
+        x = str(x).replace('(', '')
+        x = str(x).replace(',', '')
+        x = str(x).replace(')', '')
+        if x == 'None':
+            x = 0.0
+        else:
+            x = float(x)
+        return x
+    con = sqlite3.connect('settings/cde.db')
+    cur = con.cursor()
+    for row in cur.execute('''SELECT SUM(VMov) FROM movimentacao WHERE TMov='deposito' '''):
+        deposito = row
+    deposito = del_car(deposito)
+    for row in cur.execute('''SELECT SUM(VMov) FROM movimentacao WHERE TMov='saque' '''):
+        saque = row
+    saque = del_car(saque)
+    con.commit()
+    con.close()
+    return (deposito - saque)
+
 def main():
-    conta = ' R$ 1.000,00'
+    conta = rs.float_to_s(v_conta())
     ultO = '+'
     ultC = ' R$ 10,00'
-    os.system('clear')
+    os.system('cls')
     menu = '''
 {}==============================================
              {}CAIXA DE ECONOMIAS           
@@ -47,7 +82,7 @@ def main():
     rsp = str(input('{}Entre com o numero da opção:\n\n{}~/{}Terminal{} $ '.format(blue, green, yellow, cls)))
    
     if rsp == '00':
-        os.system('clear')
+        os.system('cls')
     else:
         main()
         
