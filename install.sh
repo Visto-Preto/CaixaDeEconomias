f06576c63c4756c6e7678cabb60ab94ea3fdae45
Visto-Preto <evppdepf@protonmail.ch>
2022-05-10 08:38:17 -0300

    verssion 1.0.0

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+echo -e '\n\n'
+echo -e '\033[1;31mInstalando dependências...\033[m'
+echo -e '\n'
+apt update
+apt install -y python figlet termux-api git
+
+pip install requests lolcat
+
+clear
+termux-vibrate -d 100
+figlet CaixaEco | lolcat
+echo -e '\n\n'
+echo -e '\033[1;31mInstalando o Caixa de Economias...\033[m'
+echo -e '\n'
+
+git clone https://github.com/Visto-Preto/CaixaDeEconomias.git CaixaEco
+
+cat CaixaEco/caixaeco > /data/data/com.termux/files/usr/bin/caixaeco
+chmod 700 /data/data/com.termux/files/usr/bin/caixaeco
+cp -R CaixaEco /data/data/com.termux/files/usr/share/
+rm -rf CaixaEco
+
+termux-vibrate -d 100
+echo -e '\n\n'
+echo -e 'Para iniciar o Caixa de Economias entre com o comando: \033[1;32mcaixaeco\033[m'
+echo -e '\n\n\n'
